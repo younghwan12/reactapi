ff7ef86957db9797a00e04338d94fa7bc9e0313c
younghwan12 <dowk3432@gmail.com>
2022-11-08 09:38:46 +0900

    2022.11.08

--- src/components/include/MoviePopular.jsx
+++ src/components/include/MoviePopular.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 
-function MoviePop({ popular }) {
+function MoviePop( props ) {
   return (
     <li>
-      <a href={`https://www.themoviedb.org/movie/${popular.id}`}>
+      <a href={`https://www.themoviedb.org/movie/${props.popular.id}`}>
         <img
-          src={`https://image.tmdb.org/t/p/w500/${popular.poster_path}`}
-          alt={popular.title}
+          src={`https://image.tmdb.org/t/p/w500/${props.popular.poster_path}`}
+          alt={props.popular.title}
         />
         <em>
-          <span className="title">{popular.title}</span>
-          <span className="star">{popular.vote_average}</span>
+          <span className="ranking">{props.index + 1}</span>
+          <span className="title">{props.popular.title}</span>
+          <span className="star">★ : {props.popular.vote_average}</span>
         </em>
       </a>
     </li>
@@ -22,11 +23,13 @@ const MoviePopular = (props) => {
     <section className="movie__popular">
       <div className="container">
         <div className="popular__inner">
-          <h2>극장 BOX OFFICE 순위</h2>
+          <h2>BOX OFFICE Ranking</h2>
           <ul>
-            {props.popular.map((popular, index) => (
-              <MoviePop key={index} popular={popular} />
-            ))}
+            {props.popular.map((popular, index) =>
+            index < 5 ? (
+              <MoviePop key={index} popular={popular} index = {index} />
+            ) : null
+            )}
           </ul>
         </div>
       </div>
